ed66ab5796d079e2b7795f423ad02aebfe633f07
ishax-kos <isaacbunsen@gmail.com>
2021-05-09 16:55:00 -0700

    update gitignore

--- test.d
+++ test.d
@@ -1,36 +1,22 @@
+module test;
 import std.stdio;
 
 
-immutable byte[] peepee;
-shared static this() {
-    peepee.length = 5;
-    peepee = [1, 2, 3, 4, 5];
-}
+// enum string GET_CARRY = `
+//     asm { 
+//         lahf;
+//         mov , AH;
+//     \}`;
 
+
+ubyte getCarry() {
+    ubyte ret;
+    ubyte val = 254;
+    val += 1;
+    asm {setc ret;}
+    return ret;
+}
 
 void main() {
-    // ubyte ub = 130;
-    // byte b = ub;
-    writeln(peepee);
+    writefln!"%b"(getCarry);
 }
-
-
-// ushort getU16(ubyte a, ubyte b) {
-//     return Data2(a, b).u16;
-// }
-
-
-// union Data2 {
-//     ubyte[2] u8;
-//     byte[2] s8;
-//     ushort u16;
-
-//     this(ubyte a, ubyte b) {
-//         u8 = [a, b];
-//     }
-
-//     this(ubyte[] a) {
-//         assert(a.length == 2);
-//         u8 = [a[0], a[1]];
-//     }
-// }
