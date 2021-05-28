5cc3ccb22fe27f9e9fa6d2d4708866723299de4e
ishax-kos <isaacbunsen@gmail.com>
2021-05-28 00:38:47 -0700

    lay out debugger rendering module for future

---
+++ src/debugger.d
@@ -0,0 +1,9 @@
+module debugger;
+
+import display;
+
+class GBDebugger : SubForm {
+    this(string name, uint width, uint height, bool resize) {
+        super(name, width, height, resize);
+    }
+}
